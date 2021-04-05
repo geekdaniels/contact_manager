5d9c33f0f910963a24c4bf454b7e8ab522703dc1
Olufemi Daniel <olufemidaniels07@gmail.com>
2021-04-05 11:18:47 +0100

    extend search params

--- app/app.js
+++ app/app.js
@@ -1,37 +1,40 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-        contacts: [],
-        searchParam: "",
-        message: ""
-    },
-    computed: {
-        searchContact: function () {
+  el: "#app",
+  data: {
+    contacts: [],
+    searchParam: "",
+    message: "",
+  },
+  computed: {
+    searchContact: function () {
+      var searchResults = this.contacts;
+      var searchParam = this.searchParam.toLowerCase();
+      if (!searchParam) {
+        return searchResults;
+      }
 
-            var searchResults = this.contacts
-            var searchParam = this.searchParam.toLowerCase();
-            if (!searchParam) {
-                return searchResults
-            }
-
-            searchResults = searchResults.filter(function (contact) {
-                if (contact.first_name.toLowerCase().indexOf(searchParam) !== -1 || contact.email.toLowerCase().indexOf(searchParam) !== -1) {
-                    return contact;
-                }
-            })
-
-            return searchResults;
+      searchResults = searchResults.filter(function (contact) {
+        if (
+          contact.first_name.toLowerCase().indexOf(searchParam) !== -1 ||
+          contact.last_name.toLowerCase().indexOf(searchParam) !== -1 ||
+          contact.email.toLowerCase().indexOf(searchParam) !== -1
+        ) {
+          return contact;
         }
+      });
+
+      return searchResults;
     },
-    mounted() {
-        try {
-            fetch("./MOCK_DATA.json")
-                .then(response => {
-                    return response.json();
-                })
-                .then(data => this.contacts = data);
-        } catch (error) {
-            console.error(error)
-        }
+  },
+  mounted() {
+    try {
+      fetch("./MOCK_DATA.json")
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => (this.contacts = data));
+    } catch (error) {
+      console.error(error);
     }
-})
+  },
+});
